a6de23d62bf34b1c50d4013289ecb5e836b3fcb2
Taylor <10873957@my.uvu.edu>
2021-06-05 06:04:27 -0600

    Made comment

--- Summer Class/Assets/ScriptNoteScript.cs
+++ Summer Class/Assets/ScriptNoteScript.cs
@@ -2,9 +2,8 @@ using UnityEngine;
 
 public class ScriptNoteScript : MonoBehaviour
 {
-    private void Start()
-    {
-       print("Scripts are supposed to be what Game Objects do. Behaviors of objects. The script is what they do" +
-             "How they act");
-    }
+   //Scripts are supposed to be what Game Objects do. Behaviors of objects. The script is what they do" +
+             // "How they act"
+   
+
 }
