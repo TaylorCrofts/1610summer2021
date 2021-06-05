d0efa1e7cdd2f6eb7d77feda1841552f0d9f38f5
Taylor <10873957@my.uvu.edu>
2021-06-05 05:38:13 -0600

    Added Color change and Notes Script

---
+++ Summer Class/Assets/ScriptNoteScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class ScriptNoteScript : MonoBehaviour
+{
+    private void Start()
+    {
+       print("Scripts are supposed to be what Game Objects do. Behaviors of objects. The script is what they do" +
+             "How they act");
+    }
+}
